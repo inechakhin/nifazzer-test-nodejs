3e56309439fc8ec6c115e411d27e003f760a4aef
i.nechakhin <i.nechakhin@g.nsu.ru>
2024-03-14 17:03:16 +0000

    Add lib for parsing.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { runValidationTest } from './tests/url_validation_test.js';
 import { runParserTest } from './tests/url_parser_corect_rfc_test.js';
+import { abuse } from './tests/parsing_abuse.js';
 
-runValidationTest();
+//runValidationTest();
 runParserTest();
+//abuse();
